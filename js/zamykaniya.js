766dda61ab337ebb6e9ca89c68fbb51b8860f932
Irina <i.ilchenko.spb@gmail.com>
2025-03-24 23:37:42 +0300

    task-4 ready

---
+++ js/zamykaniya.js
@@ -0,0 +1,15 @@
+// function createIdGenerator () {
+//   let lastGeneratedId = 0;
+
+//   return function () {
+//     lastGeneratedId += 1;
+//     return lastGeneratedId;
+//   };
+// }
+
+// const generatePhotoId = createIdGenerator();
+
+// console.log(generatePhotoId()); // 1
+// console.log(generatePhotoId()); // 2
+// console.log(generatePhotoId()); // 3
+
